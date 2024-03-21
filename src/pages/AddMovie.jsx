30452435c4ef5fd87d22ac8e7790a61ffe40d3e3
Brenni1 <phillip.brenndoerfer@gmail.com>
2024-03-21 16:25:56 +0100

    small bugfixes, final deploy 1.0

--- src/pages/AddMovie.jsx
+++ src/pages/AddMovie.jsx
@@ -19,7 +19,7 @@ export const AddMovie = () => {
       release_date: releaseDate,
       overview: overview,
       poster_path: posterPath,
-      genre_ids: [genre],
+      genre_ids: [Number(genre)],
     };
 
     try {
@@ -74,6 +74,21 @@ export const AddMovie = () => {
             <option value="12">Adventure</option>
             <option value="16">Animation</option>
             <option value="35">Comedy</option>
+            <option value="80">Crime</option>
+            <option value="99">Documentary</option>
+            <option value="18">Drama</option>
+            <option value="10751">Family</option>
+            <option value="14">Fantasy</option>
+            <option value="36">History</option>
+            <option value="27">Horror</option>
+            <option value="10402">Music</option>
+            <option value="9648">Mystery</option>
+            <option value="10749">Romance</option>
+            <option value="878">Sci-Fi</option>
+            <option value="10770">TV-Movie</option>
+            <option value="53">Thriller</option>
+            <option value="10752">War</option>
+            <option value="37">Western</option>
           </select>
         </label>
 
